b1de55c78224261b13ba7c40b91b0cd4fbe27b73
Danil Kilakov <gigiza1shag@mail.ru>
2025-02-06 20:08:11 +0300

    урок 3 - экземпляр класса

--- lib/steps/DartBase.dart
+++ lib/steps/DartBase.dart
@@ -1,9 +1,12 @@
+import 'package:first/ClassInstance.dart';
+
 class DartBase{
   void run() {
     print('DartBase: hello world');
 
     _stepConf();
     _stepDataType();
+    _stepClassInstance();
   }
   void _stepConf(){
     print('_stepConf: This is step 1');
@@ -18,5 +21,9 @@ class DartBase{
     print('_stepDataType: $intVar');
     print('_stepDataType: $doubleVar');
 
+  }
+  void _stepClassInstance(){
+    ClassInstance ci = new ClassInstance();
+    ci.run();
   }
 }
